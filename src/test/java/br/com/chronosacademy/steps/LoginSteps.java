6e9bf8ac506369501c81323cbbacc28a3f2d8962
Soraia Alves <soraiaalves@sisconsultoria.com.br>
2024-02-26 15:49:01 -0300

    aula 13 e 14, cumcuber e runner

---
+++ src/test/java/br/com/chronosacademy/steps/LoginSteps.java
@@ -0,0 +1,62 @@
+package br.com.chronosacademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Então("a janela modal deve ser fechada")
+    public void aJanelaModalDeveSerFechada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("for realizado um clique no ícone de fechar a modal")
+    public void forRealizadoUmCliqueNoÍconeDeFecharAModal() {
+        
+    }
+
+    @Quando("for realizado um clique no Link Create New Account")
+    public void forRealizadoUmCliqueNoLinkCreateNewAccount() {
+        
+    }
+
+    @Então("a página Create New Account deve ser exibida")
+    public void aPáginaCreateNewAccountDeveSerExibida() {
+        
+    }
+
+    @Quando("os campos de login  sejam preenchidos da seguinte forma:")
+    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma() {
+        
+    }
+
+    @Quando("for realizado o clique no botão sign in")
+    public void forRealizadoOCliqueNoBotãoSignIn() {
+        
+    }
+
+    @Então("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+        
+    }
+
+    @Então("o sistema deve exibir uma mensagem de erro")
+    public void oSistemaDeveExibirUmaMensagemDeErro() {
+        
+    }
+
+    @Então("o botão sign in deve permanecer desabilitado")
+    public void oBotãoSignInDevePermanecerDesabilitado() {
+    }
+}
